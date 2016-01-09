5bb56d3daf378aa5bf72af89f21bf8962e9166ae
Bryan <dogescript@gmail.com>
2016-01-09 16:12:30 -0700

    Optimize Compression

--- db.lua
+++ db.lua
@@ -11,18 +11,16 @@
 ]]
 
 --[[
-reservered characters:
-255-seperation character
-254-entry END
+No reserved chars! :D
 ]]
 
 os.loadAPI("specialnumber")
 
-function newDatabase(name,initialInfo)
+function newDatabase(name, numOfColumns, columnBytes, initialInfo)
     local handle = fs.open(name..".db","wb")
-    if initialInfo then
-        overwriteDatabase(handle, initialInfo)
-    end
+    local structure = {numOfColumns, columnBytes}
+    if not initialInfo then initialInfo = {} end
+    overwriteDatabase(handle, structure, initialInfo)
     handle.close()
 end
 
@@ -33,39 +31,54 @@ function writeRawToDatabase(handle, ...)
     end
 end
 
-function overwriteDatabase(handle, workingTable)
+function overwriteDatabase(handle, structure, workingTable)
+    writeRawToDatabase(handle, structure[1], unpack(structure[2]))
     for k,v in ipairs(workingTable) do
         for kk,vv in ipairs(v) do
-            local numTab = specialnumber.splitNumberToBase(vv,253)
+            local numTab = specialnumber.splitNumberToBase(vv,255)
+            for i=1,structure[2][kk]-#numTab do
+                table.insert(numTab, 1, 0)
+            end
             for kkk,vvv in ipairs(numTab) do
                 writeRawToDatabase(handle, vvv)
             end
-            writeRawToDatabase(handle, 255)
+            --writeRawToDatabase(handle, 255) <No longer needed
         end
-        writeRawToDatabase(handle, 254)
+        --writeRawToDatabase(handle, 255) <No longer needed
     end
 end
 
 function parseDatabaseToTable(name)
     local handle = fs.open(name..".db","rb")
     local workingTable = {}
+    local numOfColumns = handle.read()
+    local columnBytes = {}
+    local totalColumnBytes = 0
+    for i=1,numOfColumns do
+        table.insert(columnBytes, handle.read())
+        totalColumnBytes = totalColumnBytes + columnBytes[#columnBytes]
+    end
     local cByte = handle.read()
     local function nextB()
         cByte = handle.read()
     end
     while (cByte) do
         workingTable[#workingTable+1] = {}
-        while (cByte ~= 254) do
+        local rowBytesRead = 0
+        local columnOn = 1
+        while rowBytesRead < totalColumnBytes do
             local bytes = {}
-            while (cByte ~= 255) do
+            for i=1,columnBytes[columnOn] do
                 bytes[#bytes+1] = cByte
                 nextB()
             end
             local value = 0
             for i=1,#bytes do
-                value = value + (bytes[i] * (253^(#bytes-i)))
+                value = value + (bytes[i] * (255^(#bytes-i)))
             end
             table.insert(workingTable[#workingTable],value)
+            rowBytesRead = rowBytesRead + columnBytes[columnOn]
+            columnOn = columnOn + 1
             nextB()
         end
         nextB()
@@ -77,12 +90,11 @@ end
 function appendEntry(name, entry)
     local handle = fs.open(name..".db","ab")
     for kk,vv in ipairs(handle) do
-        local numTab = specialnumber.splitNumberToBase(vv,253)
+        local numTab = specialnumber.splitNumberToBase(vv,255)
         for kkk,vvv in ipairs(numTab) do
             writeRawToDatabase(handle, vvv)
         end
-        writeRawToDatabase(handle, 255)
     end
-    writeRawToDatabase(handle, 254)
+    --writeRawToDatabase(handle, 255) <No longer needed
     handle.close()
 end
